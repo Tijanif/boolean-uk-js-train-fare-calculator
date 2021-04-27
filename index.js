1ef5ca94d0f80725bf53506b0b707613cf4b0348
Tijani Fulani <Fulani07@gmail.com>
2021-04-27 16:14:36 +0100

    adding travel stations

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
 
+let london = 0;
+let paris = 100;
+let bacelona = 250;
+let berlin = 200;
 
 const userName = prompt("What is your name")
+const userAge = Number(prompt("How old are you?"))
 
-console.log(userName)
+alert("Welcome to Ade Station - You can travel to Bacelona, London, Paris and Berlin")
 
-const userTravelDistance = Number(prompt("How far are you traveling in Km? Price per travelled Km is £0.21")) 
-console.log(userTravelDistance)
+const userStartingStation = prompt("Where are you traveling from?").toLowerCase()
+const userDestination = prompt("Where are you traveling to?").toLocaleLowerCase()
+
+// const userTravelDistance = Number(prompt("How far are you traveling in Km? Price per travelled Km is £0.21"
+
+let userTravelDistance = 0;
+if (userStartingStation === 'london' && userDestination === "berlin" ||  userStartingStation === "berlin" && userDestination === 'london') {
+   userTravelDistance = 200;
+} else if (userStartingStation === 'paris' && userDestination === "london" ||  userStartingStation === "london" && userDestination === 'paris') {
+  userTravelDistance = 100;
+}
+
+
 
 const pricePerKm = 0.21
-const travelFare = userTravelDistance * pricePerKm
+let travelFare = userTravelDistance * pricePerKm
 
-const juniorDiscount = 0.2
-const seniorDiscount = 0.4
+// junior discount 20%
+// senior discount 40%
+const TotalAfterJuniorDiscount = 0.8
+const TotalAfterSeniorDiscount = 0.6
 
-alert(`Price per km is £0.21 you entered ${userTravelDistance} therefore you are paying £${travelFare} for your fare.`)
 
- // Junior passengers get 20% discount
- //  Senior passengers get 40% discount
+
+if (userAge < 18 ) { 
+ travelFare = travelFare * TotalAfterJuniorDiscount
+ alert(`You are traveling from ${userStartingStation} to ${userDestination} which is ${userTravelDistance}Km. Your fare price with a 20% junior discount is £${travelFare}`)
+} else if (userAge > 65){
+ travelFare = travelFare * TotalAfterSeniorDiscount
+  alert(`You are traveling from ${userStartingStation} to ${userDestination} which is ${userTravelDistance}Km. Your fare price with a 40% senior discount is £${travelFare}`)
+} else {
+ alert(`You are traveling from ${userStartingStation} to ${userDestination} which is ${userTravelDistance}Km therefore you are paying £${travelFare} for your fare.`)
+}
+
+
