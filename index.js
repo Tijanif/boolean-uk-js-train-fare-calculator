1b829a7f21d65d7b010a75ab6a0ef0cc93c16a5f
Tijani Fulani <Fulani07@gmail.com>
2021-04-27 15:05:00 +0100

    asked user for name and travel distance

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const userName = prompt("What is your name")
+
+console.log(userName)
+
+const userTravelDistance = Number(prompt("How far are you traveling in Km? Price per travelled Km is £0.21")) 
+console.log(userTravelDistance)
+
+const pricePerKm = 0.21
+const travelFare = userTravelDistance * pricePerKm
+
+
+alert(`Price per km is £0.21 you entered ${userTravelDistance} therefore you are paying £${travelFare} for your fare.`)
+
+ // Junior passengers get 20% discount
+ //  Senior passengers get 40% discount
